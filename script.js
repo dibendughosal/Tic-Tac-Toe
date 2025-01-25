8aa5e8170145106436c4b7a11bf14985953f156c
Dibendu Ghosal <128916258+dibendughosal@users.noreply.github.com>
2025-01-25 09:56:38 +0530

    Tic-Tac-Toe

---
+++ script.js
@@ -0,0 +1,60 @@
+const boxes = document.querySelectorAll('.box');
+const gameInfo = document.querySelector('.game-info');
+const newGameBtn = document.querySelector('.btn');
+
+let currentPlayer;
+const winningPosition = [
+    [0,1,2],
+    [3,4,5],
+    [6,7,8],
+    [0,3,6],
+    [1,4,7],
+    [2,5,8],
+    [0,4,8],
+    [2,4,6]
+];
+
+function initGame(){
+    currentPlayer = "X";
+    gameGrid = ["","","","","","","","",""];
+    // make empty boxes on the UI
+    boxes.forEach((box, index) => {
+        box.innerText ="";
+        boxes[index].style.pointerEvents = "all";
+    });
+    newGameBtn.classList.remove('active');
+    gameInfo.textContent = `Current Player - ${currentPlayer}`;
+
+}
+initGame();
+
+boxes.forEach((box,index) => {
+    box.addEventListener('click', () => {
+        handleClick(index);
+    });
+});
+
+function swapTurn(){
+    if(currentPlayer === "X"){
+        currentPlayer = "0";
+    }
+    else{
+        currentPlayer = "X";
+    }
+    gameInfo.textContent = `Current Player - ${currentPlayer}`;
+}
+
+function handleClick(index){
+    if(gameGrid[index] === ""){
+        boxes[index].innerText = currentPlayer; //Show in UI
+        boxes[index].style.pointerEvents = 'none';
+        gameGrid[index] = currentPlayer;
+        swapTurn();
+        checkWin();
+    }
+}
+function checkWin(){
+    newGameBtn.classList.add("active");
+}
+
+newGameBtn.addEventListener('click', initGame);
